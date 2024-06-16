66dd98c48c89cbaf6bf8b29ada38cec1c8faa8d9
Marcos Oliveira <marcosraffaeloficial@gmail.com>
2024-06-16 09:19:35 -0300

    Update 'conta_corrente' e conta_poupança'

--- src/entidade/Conta_corrente.java
+++ src/entidade/Conta_corrente.java
@@ -1,10 +1,16 @@
 package entidade;
 
 public class Conta_corrente extends Conta {
+    private boolean liberadoPoupanca;
+
     public Conta_corrente(Pessoa titular, int numConta, double saldo) {
         super(titular, numConta, saldo);
+        if (saldo >= 100)
+            this.liberadoPoupanca = true;
+        else
+            this.liberadoPoupanca = false;
 
         System.out.println("Conta corrente aberta com sucesso\nDados:");
-        System.out.println(titular);
+        System.out.println(titular.getConta().toString());
     }
 }
