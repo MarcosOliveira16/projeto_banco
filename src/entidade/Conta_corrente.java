49646fe50ada7ded04002887f8e3c12f648c436b
Marcos Oliveira <marcosraffaeloficial@gmail.com>
2024-06-16 11:43:14 -0300

    Update Conta_corrente.java

--- src/entidade/Conta_corrente.java
+++ src/entidade/Conta_corrente.java
@@ -3,7 +3,8 @@ package entidade;
 public class Conta_corrente extends Conta {
     private boolean liberadoPoupanca;
 
-    public Conta_corrente() {
+    public Conta_corrente(Pessoa titular) {
+        super(titular);
         if (getSaldo() >= 100)
             this.liberadoPoupanca = true;
         else
